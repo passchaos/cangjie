d2f542f95e898d110f571bc360b93e0b187dbf5e
passchaos <greedwolf.dss@gmail.com>
2026-08-07 15:15:58 +0800

    Add USE category foundation

--- src/use_shaper.zig
+++ src/use_shaper.zig
@@ -5,6 +5,26 @@ pub fn shouldShape(script_tag: unicode.OpenTypeScriptTag) bool {
     return script_tag == .dupl;
 }
 
+pub const Category = enum(u8) {
+    other = 0,
+    base = 1,
+    cg_joiner = 6,
+    zwnj = 14,
+    word_joiner = 16,
+    base_other = 5,
+};
+
+pub fn categoryForCodepoint(codepoint: u21) Category {
+    return switch (codepoint) {
+        0x034f => .cg_joiner,
+        0x200c => .zwnj,
+        0x2060 => .word_joiner,
+        0x1bc00...0x1bc6a => .base,
+        0x1bc70...0x1bc7c => .base_other,
+        else => .other,
+    };
+}
+
 const feature_applications = [_]gsub.FeatureApplication{
     .{ .tag = unicode.tag("locl") },
     .{ .tag = unicode.tag("ccmp") },
@@ -35,4 +55,12 @@ const feature_applications = [_]gsub.FeatureApplication{
 
 pub fn featureApplications() []const gsub.FeatureApplication {
     return &feature_applications;
+}
+
+test "USE category covers Duployan sample codepoints" {
+    try @import("std").testing.expectEqual(Category.base, categoryForCodepoint(0x1bc02));
+    try @import("std").testing.expectEqual(Category.base, categoryForCodepoint(0x1bc5b));
+    try @import("std").testing.expectEqual(Category.cg_joiner, categoryForCodepoint(0x034f));
+    try @import("std").testing.expectEqual(Category.zwnj, categoryForCodepoint(0x200c));
+    try @import("std").testing.expectEqual(Category.other, categoryForCodepoint(0x002e));
 }
